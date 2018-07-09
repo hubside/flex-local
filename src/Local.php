8e05fdc8b467190a7f16892aa91b44296fc211bc
Sebastien Malot <smalot@hubside.com>
2018-07-10 00:52:41 +0200

    Rename namespace

--- src/Local.php
+++ src/Local.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace Hubside\Flex;
+namespace Hubside\Composer\Flex;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
 
 class LocalInstallerPlugin implements PluginInterface
 {
@@ -12,6 +14,29 @@ class LocalInstallerPlugin implements PluginInterface
     {
         //$installer = new TemplateInstaller($io, $composer);
         //$composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => "processPackages",
+            ScriptEvents::POST_UPDATE_CMD => "processPackages",
+        ];
+    }
+
+    /**
+     * @param Event $event
+     * @throws \Exception
+     */
+    public function processPackages(Event $event)
+    {
+        $composer = $event->getComposer();
+        $installationManager = $composer->getInstallationManager();
+        $repositoryManager = $composer->getRepositoryManager();
+        $localRepository = $repositoryManager->getLocalRepository();
     }
 }
 
